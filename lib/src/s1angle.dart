535ae041997df87190e2c9b7fa9871553d8369f1
onur karali <onurkarali@outlook.com>
2024-02-26 00:35:20 +0100

    Make dart3 compatible

--- lib/src/s1angle.dart
+++ lib/src/s1angle.dart
@@ -49,16 +49,17 @@ class S1Angle {
 
   @override
   bool operator ==(Object other) {
-    S1Angle angle = other;
+    // ignore: test_types_in_equals
+    S1Angle angle = other as S1Angle;
     return _radians == angle._radians;
   }
 
-  bool operator <(Object other) {
+  bool operator <(S1Angle other) {
     S1Angle angle = other;
     return _radians < angle._radians;
   }
 
-  bool operator >(Object other) {
+  bool operator >(S1Angle other) {
     S1Angle angle = other;
     return _radians > angle._radians;
   }
